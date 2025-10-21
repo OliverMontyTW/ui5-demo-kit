300f5a4920f8e4648238d1e4e107e3c3ac771533
Oliver Monty <oliver.monty@teamwork.net>
2025-10-21 09:14:51 +0000

    create booking view & json model

--- webapp/controller/Booking.controller.js
+++ webapp/controller/Booking.controller.js
@@ -4,7 +4,24 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("ui5demokit.controller.Booking", {
-        onInit() {
+        onInit: function () {
+            var oModel = new sap.ui.model.json.JSONModel("../model/data.json");
+            this.getView().setModel(oModel);
+        },
+
+        onClearBooking: function (){
+            this.getView().byId("inputFullName").setValue("");
+            this.getView().byId("inputPhone").setValue("");
+            this.getView().byId("inputEmail").setValue("");
+            this.getView().byId("inputAddress").setValue("");
+            this.getView().byId("inputDateRange").setValue("");
+            this.getView().byId("selectCabinClass").setValue("Economy");
+            this.getView().byId("selectAirport").setValue("");
+        },
+
+        onSubmitBooking: function(){
+           
         }
+
     });
 });
