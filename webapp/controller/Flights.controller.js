7da67abb411ca6d00a06ace29b21c30d1efbec16
Oliver Monty <oliver.monty@teamwork.net>
2025-10-22 07:29:21 +0000

    bookings & flights details dialog

--- webapp/controller/Flights.controller.js
+++ webapp/controller/Flights.controller.js
@@ -7,6 +7,23 @@ sap.ui.define([
         onInit: function () {
             var oModel = new sap.ui.model.json.JSONModel("../model/data.json");
             this.getView().setModel(oModel);
+        },
+
+        onTilePress: function (oEvent) {
+            var oItem = oEvent.getSource();
+            var oContext = oItem.getBindingContext();
+
+            if (!this._oDialog) {
+                this._oDialog = sap.ui.xmlfragment("ui5demokit.view.FlightDetails", this);
+                this.getView().addDependent(this._oDialog);
+            }
+
+            this._oDialog.bindElement(oContext.getPath());
+            this._oDialog.open();
+        },
+        
+        onCloseDialog: function () {
+            this._oDialog.close();
         }
 
     });
